8c3e5c51f75d735d3d0a0bf9f3d317e37d39471f
brc-dd <brc-dd@users.noreply.github.com>
2021-10-09 19:55:03 +0000

    deploy: a9a2e2a4986c6f092ef3589052cd3a3c56561181

--- _next/static/GuLMrwJ5MuSYD83EmcDD1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-11ef09ed093487436e9e.js"],"/_error":["static/chunks/pages/_error-3444e87e04e8ec1fa57e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
